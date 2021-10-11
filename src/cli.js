c5be971267412a537eb73c1de73d0f84a41f3f5a
Shamil Khalikov <shamilist@mail.ru>
2021-10-11 22:28:54 +0300

    fix

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const userName = readlineSync.question('May I have your name? ');
+export const userName = () => {
+readlineSync.question('May I have your name? ');
+console.log(`Hello,${}!`);
+};
