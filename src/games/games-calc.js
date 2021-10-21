d2d1bbf3111c258b1a872e20e76b3b87f93c3e9c
Shamil Khalikov <shamilist@mail.ru>
2021-10-21 15:08:48 +0300

    add  Makefile src/even.js src/games/games-calc.js src/games/games-calc.js

--- src/games/games-calc.js
+++ src/games/games-calc.js
@@ -1,3 +1,38 @@
 import gameScript from '../index.js';
 
 const gameRules = 'What is the result of the expression?';
+
+const calculation = (num1, num2, operator) => {
+  let result = 0;
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    default:
+      break;
+  }
+  return result;
+};
+
+const operators = ['+', '-', '*'];
+
+const rundomNumber1 = Math.floor(Math.random() * 100);
+const rundomNumber2 = Math.floor(Math.random() * 100);
+const rundomOperator = Math.floor(Math.random() * 3);
+
+const gameQuestion = () => `${rundomNumber1}${operators[rundomOperator]}${rundomNumber2}`;
+
+const gamerAnswer = () => {
+  const gamerAnswer1 = String(calculation(rundomNumber1, rundomNumber2, operators[rundomOperator]));
+  return gamerAnswer1;
+};
+
+const gameCalc = () => gameScript(gameRules, gameQuestion, gamerAnswer);
+
+export default gameCalc;
