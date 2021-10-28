22420b773125c41eedb650d2516cd76c6c6c48ac
Shamil Khalikov <shamilist@mail.ru>
2021-10-28 09:08:58 +0300

    fix back

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
 const userName = () => {
-  console.log('Welcome to the Brain Games!');
   const question = readlineSync.question('May I have your name? ');
   console.log(`Hello ,${question}!`);
 };
