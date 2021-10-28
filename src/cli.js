12a956e37ba66182cfbe7d0b48d0237bcc057cb6
Shamil Khalikov <shamilist@mail.ru>
2021-10-28 09:13:31 +0300

    fix problem in src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const userName = () => {
   const question = readlineSync.question('May I have your name? ');
-  console.log(`Hello ,${question}!`);
+  console.log(`Hello, ${question}!`);
 };
 
 export default userName;
