78b13b659f2363ffa5e9a1037f4347330e91ed55
Shamil Khalikov <shamilist@mail.ru>
2021-10-28 07:00:53 +0300

    fix errors in bin/brain-games.js and src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const userName = () => {
+  console.log('Welcome to the Brain Games!');
   const question = readlineSync.question('May I have your name? ');
-  return (`Hello ,${question}!`);
+  console.log(`Hello ,${question}!`);
 };
 
 export default userName;
